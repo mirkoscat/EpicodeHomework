5787918602c5ce4250ffa3330fa3be9389dc34e0
Mirko <mirkoscat92@gmail.com>
2023-03-25 21:46:40 +0100

    added updated tic tac toe, done

--- Week3/giorno 23.03/assets/js/tictactoe.js
+++ Week3/giorno 23.03/assets/js/tictactoe.js
@@ -1,72 +1,57 @@
 "use strict";
+var Player;
+(function (Player) {
+    Player["X"] = "X";
+    Player["O"] = "O";
+    Player["Empty"] = "";
+})(Player || (Player = {}));
 class TicTacToe {
     constructor() {
-        this.currentPlayer = "X";
+        this.currentPlayer = Player.X;
         this.board = [
-            ["", "", ""],
-            ["", "", ""],
-            ["", "", ""],
+            [Player.Empty, Player.Empty, Player.Empty],
+            [Player.Empty, Player.Empty, Player.Empty],
+            [Player.Empty, Player.Empty, Player.Empty],
         ];
     }
-    checkRows() {
-        for (let i = 0; i < this.board.length; i++) {
-            if (this.board[i][0] &&
-                this.board[i][0] === this.board[i][1] &&
-                this.board[i][1] === this.board[i][2]) {
-                return this.board[i][0];
-            }
-        }
-        return undefined;
-    }
+    //ogni singola giocata
     play(coordinate) {
         const { row, col } = coordinate;
-        if (this.board[row][col] !== "") {
-            return null; // Return null if the cell is already taken
+        if (this.board[row][col] !== Player.Empty) {
+            return null; // torna null se la cella è già occupata
         }
         this.board[row][col] = this.currentPlayer;
-<<<<<<< HEAD
         const winner = this.checkWinner();
-=======
-        var winner = this.checkWinner();
         if (winner === "tie") {
-            return winner; // Return the winner if there is one
+            return winner; //placeholder per il pareggio
         }
->>>>>>> 4d4b4e47cf41cb8d0d53616a6a2451831ffee762
         if (winner) {
             return winner; // Return the winner if there is one
         }
-        this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+        this.currentPlayer = this.currentPlayer === Player.X ? Player.O : Player.X;
         return null;
     }
     checkWinner() {
         // Check rows
         for (let i = 0; i < 3; i++) {
-            if (this.board[i][0] !== "" &&
-                this.board[i][0] === this.board[i][1] &&
-                this.board[i][0] === this.board[i][2]) {
+            if (this.board[i][0] !== Player.Empty && this.board[i][0] === this.board[i][1] && this.board[i][0] === this.board[i][2]) {
                 return this.board[i][0];
             }
         }
         // Check columns
         for (let i = 0; i < 3; i++) {
-            if (this.board[0][i] !== "" &&
-                this.board[0][i] === this.board[1][i] &&
-                this.board[0][i] === this.board[2][i]) {
+            if (this.board[0][i] !== Player.Empty && this.board[0][i] === this.board[1][i] && this.board[0][i] === this.board[2][i]) {
                 return this.board[0][i];
             }
         }
         // Check diagonals
-        if (this.board[0][0] !== "" &&
-            this.board[0][0] === this.board[1][1] &&
-            this.board[0][0] === this.board[2][2]) {
+        if (this.board[0][0] !== Player.Empty && this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2]) {
             return this.board[0][0];
         }
-        if (this.board[0][2] !== "" &&
-            this.board[0][2] === this.board[1][1] &&
-            this.board[0][2] === this.board[2][0]) {
+        if (this.board[0][2] !== Player.Empty && this.board[0][2] === this.board[1][1] && this.board[0][2] === this.board[2][0]) {
             return this.board[0][2];
         }
-        // Check for tie
+        // Check pareggio
         var isTie = true;
         for (var i = 0; i < 3; i++) {
             for (var j = 0; j < 3; j++) {
@@ -83,44 +68,6 @@ class TicTacToe {
         if (isTie) {
             return "tie";
         }
-        // Return null if there is no winner
-<<<<<<< HEAD
-        return null;
-=======
-    };
-    TicTacToe.prototype.getCurrentPlayer = function () {
-        return this.currentPlayer;
-    };
-    TicTacToe.prototype.getPlayerAt = function (row, col) {
-        return this.board[row][col];
-    };
-    TicTacToe.prototype.reset = function () {
-        this.currentPlayer = "X";
-        this.board = [
-            ["", "", ""],
-            ["", "", ""],
-            ["", "", ""],
-        ];
-    };
-    return TicTacToe;
-}());
-// Instantiate a new TicTacToe game
-var game = new TicTacToe();
-// Get references to the elements in the DOM
-var boardEl = document.querySelector(".board") || null;
-var winnerEl = document.querySelector(".winner") || null;
-var playerEl = document.querySelector(".player") || null;
-playerEl.textContent = "Current player: ".concat(game.getCurrentPlayer());
-// Create the board
-for (var i = 0; i < 3; i++) {
-    for (var j = 0; j < 3; j++) {
-        var cellEl = document.createElement("div");
-        cellEl.classList.add("cell");
-        cellEl.dataset.row = i.toString();
-        cellEl.dataset.col = j.toString();
-        cellEl.addEventListener("click", handleClick);
-        boardEl.appendChild(cellEl) || null;
->>>>>>> 4d4b4e47cf41cb8d0d53616a6a2451831ffee762
     }
     getCurrentPlayer() {
         return this.currentPlayer;
@@ -154,38 +101,28 @@ function newGame() {
 }
 // Handle a click on a cell
 function handleClick(event) {
-<<<<<<< HEAD
     const row = parseInt(event.target.dataset.row);
     const col = parseInt(event.target.dataset.col);
     const winner = game.play({ row, col });
-    if (winner) {
-=======
-    var row = parseInt(event.target.dataset.row);
-    var col = parseInt(event.target.dataset.col);
-    var winner = game.play({ row: row, col: col });
     var test = document.querySelector(".reset") || null;
     console.log(winner);
     if (winner && winner !== "tie") {
->>>>>>> 4d4b4e47cf41cb8d0d53616a6a2451831ffee762
         // Display the winner
-        winnerEl.textContent = `Winner: ${winner}`;
+        winnerEl.textContent = "Winner: ".concat(winner);
         playerEl.textContent = "";
-        test.classList.toggle("hidden");
-        console.log(test.classList.contains("hidden"));
+        test === null || test === void 0 ? void 0 : test.classList.toggle("hidden");
+        console.log(test === null || test === void 0 ? void 0 : test.classList.contains("hidden"));
     }
     else if (winner === "tie") {
         // Display the winner
         winnerEl.textContent = "It's a tie!";
         playerEl.textContent = "";
-        test.classList.toggle("hidden");
+        test === null || test === void 0 ? void 0 : test.classList.toggle("hidden");
     }
     else {
         // Display the current player
-<<<<<<< HEAD
-        playerEl.textContent = `Current player: ${game.getCurrentPlayer()}`;
-=======
+        playerEl.textContent = "Current player: ".concat(game.getCurrentPlayer());
         console.log(winner);
->>>>>>> 4d4b4e47cf41cb8d0d53616a6a2451831ffee762
     }
     // Update the board
     updateBoard();
@@ -206,7 +143,6 @@ function updateBoard() {
         }
     });
 }
-<<<<<<< HEAD
 // Start a new game when the page loads
 newGame();
 // Add a click event listener to the Refresh button
@@ -216,13 +152,4 @@ function handleRefresh() {
     playerEl.textContent = `Current player: ${game.getCurrentPlayer()}`;
     winnerEl.textContent = "";
     updateBoard();
-=======
-function reset() {
-    game.reset();
-    updateBoard();
-    var test = document.querySelector(".reset") || null;
-    test.classList.toggle("hidden");
-    winnerEl.textContent = "";
-    playerEl.textContent = "Current player: ".concat(game.getCurrentPlayer());
->>>>>>> 4d4b4e47cf41cb8d0d53616a6a2451831ffee762
 }
--- Week3/giorno 23.03/assets/ts/tictactoe.ts
+++ Week3/giorno 23.03/assets/ts/tictactoe.ts
@@ -1,203 +1,77 @@
-
+enum Player { X = "X", O = "O", Empty = "" }
 class TicTacToe {
-<<<<<<< HEAD
+
   public currentPlayer: any;
   public board: any;
   public row: any;
   public col: any;
-=======
-    public currentPlayer: any;
-    public board: any;
-    public row: any;
-    public col: any;
->>>>>>> 4d4b4e47cf41cb8d0d53616a6a2451831ffee762
 
   constructor() {
-    this.currentPlayer = "X";
+
+    this.currentPlayer = Player.X;
     this.board = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""],
+      [Player.Empty,Player.Empty,Player.Empty],
+      [Player.Empty, Player.Empty,Player.Empty],
+      [Player.Empty,Player.Empty, Player.Empty],
     ];
   }
-  checkRows() {
-    for (let i = 0; i < this.board.length; i++) {
-      if (this.board[i][0] &&
-        this.board[i][0] === this.board[i][1] &&
-        this.board[i][1] === this.board[i][2]) {
-        return this.board[i][0];
-      }
-    }
-    return undefined;
-  }
+
+  //ogni singola giocata
   play(coordinate: any) {
     const { row, col } = coordinate;
-    if (this.board[row][col] !== "") {
-      return null; // Return null if the cell is already taken
+    if (this.board[row][col] !== Player.Empty) {
+      return null; // torna null se la cella è già occupata
     }
-<<<<<<< HEAD
     this.board[row][col] = this.currentPlayer;
     const winner = this.checkWinner();
+    if (winner === "tie") {
+      return winner; //placeholder per il pareggio
+    }
     if (winner) {
       return winner; // Return the winner if there is one
     }
-    this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+    this.currentPlayer = this.currentPlayer === Player.X ? Player.O : Player.X;
     return null;
   }
+
   checkWinner() {
     // Check rows
     for (let i = 0; i < 3; i++) {
-      if (this.board[i][0] !== "" &&
-        this.board[i][0] === this.board[i][1] &&
-        this.board[i][0] === this.board[i][2]) {
+      if (this.board[i][0] !== Player.Empty && this.board[i][0] === this.board[i][1] && this.board[i][0] === this.board[i][2]) {
         return this.board[i][0];
       }
-=======
-    play(coordinate) {
-        const { row, col } = coordinate;
-        if (this.board[row][col] !== "") {
-            return null; // Return null if the cell is already taken
-        }
-        this.board[row][col] = this.currentPlayer;
-        const winner = this.checkWinner();
-        if (winner === "tie") {
-            return winner; // Return the winner if there is one
-        }
-        if (winner) {
-            return winner; // Return the winner if there is one
-        }
-        this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
-        return null;
-    }
-    checkWinner() {
-        // Check rows
-        for (let i = 0; i < 3; i++) {
-            if (this.board[i][0] !== "" &&
-                this.board[i][0] === this.board[i][1] &&
-                this.board[i][0] === this.board[i][2]) {
-                return this.board[i][0];
-            }
-        }
-        // Check columns
-        for (let i = 0; i < 3; i++) {
-            if (this.board[0][i] !== "" &&
-                this.board[0][i] === this.board[1][i] &&
-                this.board[0][i] === this.board[2][i]) {
-                return this.board[0][i];
-            }
-        }
-        // Check diagonals
-        if (this.board[0][0] !== "" &&
-            this.board[0][0] === this.board[1][1] &&
-            this.board[0][0] === this.board[2][2]) {
-            return this.board[0][0];
-        }
-        if (this.board[0][2] !== "" &&
-            this.board[0][2] === this.board[1][1] &&
-            this.board[0][2] === this.board[2][0]) {
-            return this.board[0][2];
-        }
-
-        // Check for tie
-        let isTie = true;
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                if (this.board[i][j] === "") {
-                    isTie = false;
-                    break;
-                }
-            }
-            if (!isTie) {
-                return null;
-                break;
-            }
-        }
-        if (isTie) {
-            return "tie";
-        }
-
-
-        // Return null if there is no winner
-    }
-    getCurrentPlayer() {
-        return this.currentPlayer;
-    }
-    getPlayerAt(row, col) {
-        return this.board[row][col];
-    }
-    reset() {
-        this.currentPlayer = "X";
-        this.board = [
-            ["", "", ""],
-            ["", "", ""],
-            ["", "", ""],
-        ];
-    }
-    }
-// Instantiate a new TicTacToe game
-const game = new TicTacToe();
-// Get references to the elements in the DOM
-const boardEl = document.querySelector(".board") || null;
-const winnerEl = document.querySelector(".winner") || null;
-const playerEl = document.querySelector(".player") || null;
-playerEl.textContent = `Current player: ${game.getCurrentPlayer()}`
-// Create the board
-for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-        const cellEl = document.createElement("div");
-        cellEl.classList.add("cell");
-        cellEl.dataset.row = i.toString();
-        cellEl.dataset.col = j.toString();
-        cellEl.addEventListener("click", handleClick);
-        boardEl.appendChild(cellEl) || null;
-    }
-}
-// Handle a click on a cell
-function handleClick(event) {
-    const row = parseInt(event.target.dataset.row);
-    const col = parseInt(event.target.dataset.col);
-    const winner = game.play({ row, col });
-    const test = document.querySelector(".reset") || null;
-    console.log(winner)
-    if (winner && winner !== "tie") {
-        // Display the winner
-        winnerEl.textContent = `Winner: ${winner}`;
-        playerEl.textContent = "";
-        test.classList.toggle("hidden");
-        console.log(test.classList.contains("hidden"))
-
-    } else if (winner === "tie") {
-        // Display the winner
-        winnerEl.textContent = `It's a tie!`;
-        playerEl.textContent = "";
-        test.classList.toggle("hidden");
-    }
-    else {
-        // Display the current player
-        console.log(winner)
->>>>>>> 4d4b4e47cf41cb8d0d53616a6a2451831ffee762
     }
     // Check columns
     for (let i = 0; i < 3; i++) {
-      if (this.board[0][i] !== "" &&
-        this.board[0][i] === this.board[1][i] &&
-        this.board[0][i] === this.board[2][i]) {
+      if (this.board[0][i] !== Player.Empty &&this.board[0][i] === this.board[1][i] &&this.board[0][i] === this.board[2][i]) {
         return this.board[0][i];
       }
     }
     // Check diagonals
-    if (this.board[0][0] !== "" &&
-      this.board[0][0] === this.board[1][1] &&
-      this.board[0][0] === this.board[2][2]) {
+    if (this.board[0][0] !== Player.Empty &&this.board[0][0] === this.board[1][1] &&this.board[0][0] === this.board[2][2]) {
       return this.board[0][0];
     }
-    if (this.board[0][2] !== "" &&
-      this.board[0][2] === this.board[1][1] &&
-      this.board[0][2] === this.board[2][0]) {
+    if (this.board[0][2] !== Player.Empty && this.board[0][2] === this.board[1][1] &&this.board[0][2] === this.board[2][0]) {
       return this.board[0][2];
     }
-    // Return null if there is no winner
-    return null;
+    // Check pareggio
+    var isTie = true;
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        if (this.board[i][j] === "") {
+          isTie = false;
+          break;
+        }
+      }
+      if (!isTie) {
+        return null;
+        break;
+      }
+    }
+    if (isTie) {
+      return "tie";
+    }
+
   }
   getCurrentPlayer() {
     return this.currentPlayer;
@@ -205,6 +79,8 @@ function handleClick(event) {
   getPlayerAt(row: any, col: any) {
     return this.board[row][col];
   }
+
+  
 }
 
 // Get references to the elements in the DOM
@@ -240,13 +116,24 @@ function handleClick(event: any) {
   const row = parseInt(event.target.dataset.row);
   const col = parseInt(event.target.dataset.col);
   const winner = game.play({ row, col });
-  if (winner) {
+  var test = document.querySelector(".reset") || null;
+  console.log(winner);
+  if (winner && winner !== "tie") {
     // Display the winner
-    winnerEl.textContent = `Winner: ${winner}`;
+    winnerEl.textContent = "Winner: ".concat(winner);
     playerEl.textContent = "";
+    test?.classList.toggle("hidden");
+    console.log(test?.classList.contains("hidden"));
+  }
+  else if (winner === "tie") {
+    // Display the winner
+    winnerEl.textContent = "It's a tie!";
+    playerEl.textContent = "";
+    test?.classList.toggle("hidden");
   } else {
     // Display the current player
-    playerEl.textContent = `Current player: ${game.getCurrentPlayer()}`;
+    playerEl.textContent = "Current player: ".concat(game.getCurrentPlayer());
+    console.log(winner);
   }
   // Update the board
   updateBoard();
@@ -254,7 +141,6 @@ function handleClick(event: any) {
 
 // Update the board
 function updateBoard() {
-<<<<<<< HEAD
   const cells = boardEl.querySelectorAll(".cell") as NodeListOf<HTMLDivElement>;
   cells.forEach((cell) => {
     const row = parseInt(cell.dataset.row ?? "");
@@ -279,28 +165,4 @@ function handleRefresh() {
   playerEl.textContent = `Current player: ${game.getCurrentPlayer()}`;
   winnerEl.textContent = "";
   updateBoard();
-=======
-    const cells = boardEl.querySelectorAll(".cell") as NodeListOf<HTMLDivElement>;
-    cells.forEach((cell) => {
-        const row = parseInt(cell.dataset.row);
-        const col = parseInt(cell.dataset.col);
-        const player = game.getPlayerAt(row, col);
-        if (player) {
-            cell.textContent = player;
-        }
-        else {
-            cell.textContent = "";
-        }
-    }
-    );
-}
-
-function reset() {
-    game.reset();
-    updateBoard();
-    const test = document.querySelector(".reset") || null;
-    test.classList.toggle("hidden");
-    winnerEl.textContent = "";
-    playerEl.textContent = `Current player: ${game.getCurrentPlayer()}`;
->>>>>>> 4d4b4e47cf41cb8d0d53616a6a2451831ffee762
 }
